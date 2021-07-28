88ef77eabcf73da5ab6dffce08626058e9303155
Hui Lyu <lvhuiangle@gmail.com>
2021-07-28 11:40:46 -0400

    working on milestone 3

--- app/src/main/java/algonquin/cst2335/hhsrandroidproject1/chargingStation/ChargingStation.java
+++ app/src/main/java/algonquin/cst2335/hhsrandroidproject1/chargingStation/ChargingStation.java
@@ -1,13 +1,29 @@
 package algonquin.cst2335.hhsrandroidproject1.chargingStation;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.navigation.NavigationView;
+
+import algonquin.cst2335.hhsrandroidproject1.MainActivity;
 import algonquin.cst2335.hhsrandroidproject1.R;
 
 
@@ -19,7 +35,7 @@ public class ChargingStation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.empty_layout);
+        setContentView(R.layout.charging_station_empty_layout);
         sharedPref = getSharedPreferences("ChargingStation", MODE_PRIVATE);
         isTablet = findViewById(R.id.detailsRoom) != null;
 
@@ -34,8 +50,7 @@ public class ChargingStation extends AppCompatActivity {
     public void userClickedStation(ChargingStationPOJO station, int position) {
 
         ChargingStationDetailsFragment detailsFragment = new ChargingStationDetailsFragment(station, position);
-        //   ChargingStationFavouriteStation favouriteStation = new ChargingStationFavouriteStation();
-    //    favouriteStation.setPositionClicked(position);
+
         if(isTablet)
         {
             //A tablet has a second Fragment with id detailsRoom to load a second fragment
@@ -47,9 +62,38 @@ public class ChargingStation extends AppCompatActivity {
         }
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.charging_station_activity_actions, menu);
+        return true;
+    }
     public void notifyStationDeleted(ChargingStationPOJO chosenStation, int chosenPosition) {
         stationFragment.notifyStationDeleted(chosenStation, chosenPosition);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
 
--- app/src/main/java/algonquin/cst2335/hhsrandroidproject1/chargingStation/ChargingStationListFragment.java
+++ app/src/main/java/algonquin/cst2335/hhsrandroidproject1/chargingStation/ChargingStationListFragment.java
@@ -2,12 +2,16 @@ package algonquin.cst2335.hhsrandroidproject1.chargingStation;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -16,11 +20,17 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import org.json.JSONArray;
@@ -35,6 +45,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+import algonquin.cst2335.hhsrandroidproject1.MainActivity;
 import algonquin.cst2335.hhsrandroidproject1.R;
 
 public class ChargingStationListFragment extends Fragment {
@@ -61,24 +72,16 @@ public class ChargingStationListFragment extends Fragment {
         latitude = StationLayout.findViewById(R.id.latitudeInput);
         longitude = StationLayout.findViewById(R.id.longitudeInput);
         search = StationLayout.findViewById(R.id.searchButton);
-        Button help = StationLayout.findViewById(R.id.help);
 
         adt = new ChargingStationAdapter(stationList);
         stations.setAdapter(adt);
         stations.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         sharedPref = getContext().getSharedPreferences("ChargingStation", Context.MODE_PRIVATE);
         latitude.setText(sharedPref.getString("Latitude", ""));
-        longitude.setText(sharedPref.getString("Longitude",""));
-        search.setOnClickListener(clk->{
-           // longitude.onEditorAction(EditorInfo.IME_ACTION_DONE);
-            //Intent nextPage = new Intent(this, ChargingStationDetails.class);
-//            for(int i = 0; i<=10;i++){
-//                ChargingStationPOJO pojo = new ChargingStationPOJO("a",2.22, 3.33,"123456");
-//                pojo.setTitle("i" + i);
-//                stationList.add(pojo);
-//            }
+        longitude.setText(sharedPref.getString("Longitude", ""));
+        search.setOnClickListener(clk -> {
             String url = "https://api.openchargemap.io/v3/poi/?key=aa49870e-45ee-4c7d-aa07-bad1b3e0e07a&output=json&countrycode=CA&latitude="
-                    + latitude.getText().toString() + "&longitude=" + longitude.getText().toString() + "&maxresults=2";
+                    + latitude.getText().toString() + "&longitude=" + longitude.getText().toString() + "&maxresults=5";
             DownloadFilesTask downloadFileTask = new DownloadFilesTask();
             downloadFileTask.execute(url);
 
@@ -90,27 +93,81 @@ public class ChargingStationListFragment extends Fragment {
 
         });
 
-        help.setOnClickListener(helpClk->{
-            AlertDialog.Builder builder = new AlertDialog.Builder( getContext() );
+        Toolbar myToolbar = StationLayout.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(myToolbar);
+//        setSupportActionBar(myToolbar);
+        DrawerLayout drawer = StationLayout.findViewById(R.id.drawer_layout);
 
-            builder.setMessage("Directly input the latitude and longitude would be fine")
-                    .setTitle("Instruction")
-                    .setCancelable(false)
-                    .setPositiveButton("OK", (dialog, cl) -> { })
-                    .create().show();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), drawer, myToolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        NavigationView navigationView = StationLayout.findViewById(R.id.popout_menu);
+        navigationView.setNavigationItemSelectedListener((item) -> {
+            onOptionsItemSelected(item);
+            drawer.closeDrawer(GravityCompat.START);
+            return false;
         });
-
         return StationLayout;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+//            case R.id.bus_home:
+//
+//                break;
+            case R.id.car_charging_home:
+                startChargingStationActivity();
+                break;
+//            case R.id.movie_home:
+//
+//                break;
+//            case R.id.soccer_home:
+
+//                break;
+            case R.id.help:
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setMessage("Directly input the latitude and longitude would be fine")
+                        .setTitle("Instruction")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", (dialog, cl) -> {
+                        })
+                        .create()
+                        .show();
+                break;
+            case R.id.home:
+                HomeActivity();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.charging_station_activity_actions, menu);
+
+        //super.onCreateOptionsMenu(menu, inflater);
+    }
+    public void startChargingStationActivity() {
+        Intent chargingStation = new Intent(getContext(), ChargingStation.class);
+        startActivity(chargingStation);
+    }
+
+    public void HomeActivity() {
+        Intent home = new Intent(getContext(), MainActivity.class);
+        startActivity(home);
+    }
 
 
     public void notifyStationDeleted(ChargingStationPOJO chosenStation, int chosenPosition) {
-        AlertDialog.Builder builder = new AlertDialog.Builder( getContext() );
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         builder.setMessage("are you sure you want to delete this station: " + chosenStation.getTitle())
                 .setTitle("Danger!")
-                .setNegativeButton("Cancel", (dialog, cl) -> { })
+                .setNegativeButton("Cancel", (dialog, cl) -> {
+                })
                 .setPositiveButton("Delete", (dialog, cl) -> {
 
                     //position = getAbsoluteAdapterPosition();
@@ -119,13 +176,13 @@ public class ChargingStationListFragment extends Fragment {
                     stationList.remove(chosenPosition);
                     adt.notifyItemRemoved(chosenPosition);
 
-                    db.delete(ChargingOpenHelper.TABLE_NAME, "_id=?", new String[] { Long.toString(removedStation.getId()) });
+                    db.delete(ChargingOpenHelper.TABLE_NAME, "_id=?", new String[]{Long.toString(removedStation.getId())});
 
 
                     Snackbar.make(search, "You deleted message #" + chosenPosition, Snackbar.LENGTH_LONG)
                             .setAction("Undo", clk -> {
 
-                                stationList.add(chosenPosition,  removedStation);
+                                stationList.add(chosenPosition, removedStation);
                                 adt.notifyItemInserted(chosenPosition);
                                 db.execSQL("Insert into " + ChargingOpenHelper.TABLE_NAME + " values('" + removedStation.getId() +
                                         "','" + removedStation.getTitle() +
@@ -138,6 +195,7 @@ public class ChargingStationListFragment extends Fragment {
                 .create().show();
     }
 
+
     /**
      * Class connects to the server, reads and process the data
      */
@@ -147,30 +205,28 @@ public class ChargingStationListFragment extends Fragment {
          */
         private ProgressDialog p;
         //AlertDialog dialog;
+
         /**
          * Method shows a progression of downloading data from the server to the user
          */
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-
-//            AlertDialog dialog = new AlertDialog.Builder(getContext())
-//                    .setTitle("Getting Station")
-//                    .setMessage("Please waiting")
-//                    .setView(new ProgressBar(getContext()))
-//                    .show();
             p = new ProgressDialog(getContext());
             p.setTitle("Getting Station");
             p.setMessage("Please wait...");
             p.setCancelable(false);
             p.show();
         }
+
         /**
          * Methods connects to the server, retrieves the data about car charging stations
+         *
          * @param urls link to the server
          * @return data about car charging stations
          */
         protected String doInBackground(String... urls) {
+
             HttpURLConnection urlConnection = null;
             String result = "";
             try {
@@ -209,8 +265,10 @@ public class ChargingStationListFragment extends Fragment {
             }
             return result;
         }
+
         /**
          * Method gets car charging stations and displays them on the screen as a list of items
+         *
          * @param result data about car charging stations retrieved by doInBackground method()
          */
         protected void onPostExecute(String result) {
@@ -223,7 +281,7 @@ public class ChargingStationListFragment extends Fragment {
             }
             try {
                 JSONArray jsonArray = new JSONArray(result);
-                for(int i = 0; i < jsonArray.length(); i++){
+                for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject stationJSON = jsonArray.getJSONObject(i);
                     JSONObject addressJSON = stationJSON.getJSONObject("AddressInfo");
 
@@ -237,12 +295,11 @@ public class ChargingStationListFragment extends Fragment {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-           // ChargingStationAdapter adapter = new ChargingStationAdapter(getApplicationContext(), stationList, false);
-           // theList.setAdapter(adapter);
+            // ChargingStationAdapter adapter = new ChargingStationAdapter(getApplicationContext(), stationList, false);
+            // theList.setAdapter(adapter);
         }
 
     }
-
 
 
 }
