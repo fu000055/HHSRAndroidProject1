495d71832910773394a858d8aa80fa700283fc2a
simondaxia <abcde>
2021-08-04 07:14:10 -0400

    MovieInformation finished milestone 4_InfoOpen2

---
+++ app/src/main/java/algonquin/cst2335/hhsrandroidproject1/MovieIfo/MovieAddInfoFragment.java
@@ -0,0 +1,7 @@
+package algonquin.cst2335.hhsrandroidproject1.MovieIfo;
+
+import androidx.fragment.app.Fragment;
+
+public class MovieAddInfoFragment extends Fragment {
+
+}
---
+++ app/src/main/java/algonquin/cst2335/hhsrandroidproject1/MovieIfo/MovieAddInfoListFragment.java
@@ -0,0 +1,21 @@
+package algonquin.cst2335.hhsrandroidproject1.MovieIfo;
+
+import androidx.fragment.app.Fragment;
+
+public class MovieAddInfoListFragment extends Fragment {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
